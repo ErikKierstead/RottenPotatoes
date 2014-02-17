03a2728bc3f0613014663a8caf0001c1c6dd1536
Erik Kierstead <ErikKierstead@gmail.com>
2014-02-17 07:09:54 -0400

    Added Link to Movie Title

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,7 +14,12 @@ class MoviesController < ApplicationController
 
   def index
      #@movies = Movie.all
-     @movies = Movie.order(params[:sort])
+    #@movies = Movie.order(params[:sort])
+    @movies = Movie.all
+#@movies = Movie.order(params[:sort])
+    if(params[:sort].to_s == 'title')
+       session[:sort] = params[:sort]
+       @movies = @movies.sort_by{|m| m.title}
   end
 
   def new
